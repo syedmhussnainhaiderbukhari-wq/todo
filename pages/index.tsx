ed3d6a09f12713d307ee9d76d57f825486536e95
syedmhussnainhaiderbukhari-wq <syedm.hussnainhaiderbukhari@gmail.com>
2026-03-19 15:46:33 +0500

    Create HomePage component for the todo app

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Welcome to Todo App!</h1>
+            <p>Manage your tasks efficiently.</p>
+        </div>
+    );
+};
+
+export default HomePage;
