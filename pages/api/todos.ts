3e06f88bacdce855d965612f0d2dc1dd4eac82e3
syedmhussnainhaiderbukhari-wq <syedm.hussnainhaiderbukhari@gmail.com>
2026-03-19 15:49:03 +0500

    Create API route handler for managing todos

---
+++ pages/api/todos.ts
@@ -0,0 +1,22 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+let todos = [];
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method === 'GET') {
+        // Return the list of todos
+        res.status(200).json(todos);
+    } else if (req.method === 'POST') {
+        // Add a new todo
+        const { todo } = req.body;
+        if (!todo) {
+            return res.status(400).json({ message: 'Todo is required' });
+        }
+        todos.push(todo);
+        res.status(201).json({ message: 'Todo added', todo });
+    } else {
+        // Handle any other HTTP method
+        res.setHeader('Allow', ['GET', 'POST']);
+        res.status(405).end(`Method ${req.method} Not Allowed`);
+    }
+}
